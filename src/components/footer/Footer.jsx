d5f4798f728f57a23fed629b76e803b19e889075
iamus4ma <135535679+iamus4ma@users.noreply.github.com>
2023-10-07 16:27:00 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__container container">
-        <h1 className="footer__title"> Usama</h1>
+        <h1 className="footer__title">IAMUS4MA</h1>
 
         <ul className="footer__list">
           <li>
