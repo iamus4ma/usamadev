32881f92c241a64c264670cfc563b5b2959664a7
iamus4ma <iamus4ma@gmail.com>
2024-08-21 13:54:20 +0500

    chat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, { useEffect } from 'react';
 import './App.css';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
@@ -10,21 +10,36 @@ import Scrollup from './components/scrollup/Scrollup';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 import Testimonial from './components/testimonials/Testimonial';
+
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.tawk.to/66c5a601ea492f34bc0870c5/1i5q10ubl";
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Clean up the script when the component unmounts
+    };
+  }, []);
+
   return (
     <>
-    <Header />
-    <main className='main'>
-    <Home />
-    <About />
-    <Skills />
-    <Services />
-    <Qualification />
-    <Testimonial />
-    <Contact />
-    </main>
-    <Footer />
-    <Scrollup />
+      <Header />
+      <main className='main'>
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <Testimonial />
+        <Contact />
+      </main>
+      <Footer />
+      <Scrollup />
     </>
   );
 }
--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Scrollup.css";
 
 const Scrollup = () => {
-  window.addEventListener("scroll", function () {
-    const scrollUp = document.querySelector(".scrollup");
-    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-    else scrollUp.classList.remove("show-scroll");
-  });
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = document.querySelector(".scrollup");
+      if (window.scrollY >= 560) {
+        scrollUp.classList.add("show-scroll");
+      } else {
+        scrollUp.classList.remove("show-scroll");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <a href="" className="scrollup">
+    <a href="#top" className="scrollup">
       <i className="uil uil-arrow-up scrollup__icon"></i>
     </a>
   );
